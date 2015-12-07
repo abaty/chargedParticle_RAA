90011224c8cdb1dad8970d49d16e9b94a262bcc1
Austin <abaty@mit.edu>
2015-12-07 17:37:59 -0500

    updated datasets and some changes to the count tracks macro

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ fi
 
 sleep $(($1/2))
 
-echo | awk -v j=$1 -v k=$2 '{print "./run.exe MinBiasPD_1_6.txt "j" "k}' 
-echo | awk -v j=$1 -v k=$2 '{print "./run.exe MinBiasPD_1_6.txt "j" "k}' | bash
+echo | awk -v j=$1 -v k=$2 '{print "./run.exe fileList.txt "j" "k}' 
+echo | awk -v j=$1 -v k=$2 '{print "./run.exe fileList.txt "j" "k}' | bash
 
 echo | awk -v tag=$4 -v user=$USER '{print "mv output*.root /mnt/hadoop/cms/store/user/"user"/temporaryStorage/"}'
 echo | awk -v tag=$4 -v user=$USER '{print "mv output*.root /mnt/hadoop/cms/store/user/"user"/temporaryStorage/"}' | bash
