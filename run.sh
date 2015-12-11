759ea80005c33142b632ea0fb8ad07b8a5c90069
Austin <abaty@mit.edu>
2015-12-11 04:13:51 -0500

    updated output move command bug

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ sleep $(($1/2))
 echo | awk -v j=$1 -v k=$2 '{print "./run.exe fileList.txt "j" "k}' 
 echo | awk -v j=$1 -v k=$2 '{print "./run.exe fileList.txt "j" "k}' | bash
 
-echo | awk -v tag=$4 -v user=$USER '{print "mv output*.root /mnt/hadoop/cms/store/user/"user"/temporaryStorage/"}'
-echo | awk -v tag=$4 -v user=$USER '{print "mv output*.root /mnt/hadoop/cms/store/user/"user"/temporaryStorage/"}' | bash
+echo | awk -v tag=$4 -v user=$USER '{print "mv *output*.root /mnt/hadoop/cms/store/user/"user"/temporaryStorage/"}'
+echo | awk -v tag=$4 -v user=$USER '{print "mv *output*.root /mnt/hadoop/cms/store/user/"user"/temporaryStorage/"}' | bash
 rm *.root
 echo "job done successfully"
