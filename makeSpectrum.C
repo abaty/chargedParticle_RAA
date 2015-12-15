cbcd77b6e4c40ac75be1c10696949dca25190bef
Austin <abaty@mit.edu>
2015-12-15 13:05:56 -0500

    added new corrections, added some plotting macros

--- makeSpectrum.C
+++ makeSpectrum.C
@@ -197,7 +197,6 @@ void makeSpectrum()
   c1->SaveAs("plots/JetRelativeTurnOnes.png");
   c1->SaveAs("plots/JetRelativeTurnOnes.pdf");
   
-
   c1->SetLogy();
   c1->SetLogx();
   pp->SetMarkerSize(0.8);
@@ -221,4 +220,44 @@ void makeSpectrum()
    
   c1->SaveAs("plots/ppTrack_FullSpectrum.png");
   c1->SaveAs("plots/ppTrack_FullSpectrum.pdf");
+
+  //RpPb bin shift correction
+/*  TFile * binShift = TFile::Open("BinningAndResolutionCorrection_TrackTrigger.root","read");
+  TH1D *hBinningAndResol_extendedPt = (TH1D*)f_binningAndResol_extendedPt->Get("hPt_pseudo2_copy1");
+  hBinningAndResol_extendedPt->SetDirectory(0);
+  binShift->Close();
+  TH1D *pp_BinShift = (TH1D*)pp->Clone("pp_BinShift"); 
+  for(int i = 2; i<hBinningAndResol_extendedPt->GetSize()+1; i++)
+  {
+    pp_BinShift->SetBinContent(i-1,hBinningAndResol_extendedPt->GetBinContent(i));
+    pp_BinShift->SetBinError(i-1,hBinningAndResol_extendedPt->GetBinError(i));
+  }
+  for(int i = 1; i<pp_binShift->GetSize()+1; i++)
+  {
+    if(pp_binShift
+  }*/
+
+  //interpolation points
+  float ppintyvals[s.ntrkBins+1] = {4.1202/0.5946*0.999, 2.8116/0.6211*1.003, 1.9778/0.6552*1.002, 1.4330/0.6984*0.994 , 1.0405/0.7219*1.002, 0.7719/0.7515*1.002, 0.5816/0.7809*1.002, 0.3893/0.825*1.001, 0.23381/0.866*1.000, 0.14395/0.901*1.004, 0.09103/0.925*1.005, 0.05937/0.965*0.997, 0.03906/0.984*0.998, 0.014787/1.023*1.062, 0.003806/1.052*1.042, 0.001181/1.056*1.033, 0.0004290/1.048*1.024, 0.0001787/1.054*1.018, 0.00008152/1.031*1.015, 0.00002216/1.023* 1.109, 0.000004653/1.036*1.061, 0.000001402/1.054*1.040, 0.0000003180/1.072*1.111, 0.00000006850/1.142*1.065, 0.00000001971/1.189*1.044, 0.000000006013/1.259*1.051, 0.000000001910/1.308*1.033, 0.0000000007181/1.342*1.024, 0.0000000002083/1.382*1.078,0.00000000005311/1.407*1.05,0.00000000001639/1.363*1.034386,0.000000000005354/1.381*1.047316,0.000000000001709/1.316*1.032760,0,0,0,0,0,0,0,0,0 };
+  float ppintyvals_pPb[s.ntrkBins+1] = {4.1202*0.999, 2.8116*1.003, 1.9778*1.002, 1.4330*0.994 , 1.0405*1.002, 0.7719*1.002, 0.5816*1.002, 0.3893*1.001, 0.23381*1.000, 0.14395*1.004, 0.09103*1.005, 0.05937*0.997, 0.03906*0.998, 0.014787*1.062, 0.003806*1.042, 0.001181*1.033, 0.0004290*1.024, 0.0001787*1.018, 0.00008152*1.015, 0.00002216* 1.109, 0.000004653*1.061, 0.000001402*1.040, 0.0000003180*1.111, 0.00000006850*1.065, 0.00000001971*1.044, 0.000000006013*1.051, 0.000000001910*1.033, 0.0000000007181*1.024, 0.0000000002083*1.078,0.00000000005311*1.05,0.00000000001639*1.034386,0.000000000005354*1.047316,0.000000000001709*1.032760,0,0,0,0,0,0,0,0,0 };
+  float ppintyvals_RpPb[s.ntrkBins+1] = {0.5946, 0.6211, 0.6552, 0.6984,0.7219,0.7515, 0.7809, 0.825, 0.866, 0.901, 0.925, 0.965, 0.984, 1.023, 1.052,1.056, 1.048, 1.054, 1.031, 1.023, 1.036, 1.054, 1.072, 1.142, 1.189, 1.259, 1.308, 1.342,1.382,1.407,1.363,1.381,1.316,0,0,0,0,0,0,0,0,0 };
+  float ppintyerrRpPb[s.ntrkBins+1] = {0.0027/0.5946,0.0028/0.6211,0.0030/0.6552,0.0032/0.6984,0.0033/0.7219,0.0034/0.7515,0.0036/0.7809,0.003/0.825,0.003/0.866,0.003/0.901,0.003/0.925,0.003/0.965,0.003/0.984,0.002/1.023,0.002/1.052,0.002/1.056,0.002/1.048,0.003/1.054,0.003/1.031,0.003/1.023,0.005/1.036,0.008/1.054,0.001/1.072,0.002/1.142,0.001/1.189,0.002/1.259,0.002/1.308,0.004/1.342,0.004/1.382,0.008/1.407,0.013/1.363,0.019/1.381,0.030/1.316,0,0,0,0,0,0,0,0,0 };
+  float ppintyerr_pPb[s.ntrkBins+1] = {0.0188/4.1202,0.0128/2.8116,0.0090/1.9778,0.0065/1.4330,0.0047/1.0405,0.0035/0.7719,0.0026/0.5816,0.0013/0.3893,0.00076/0.23381,0.00047/0.14395,0.00029/0.09103,0.00019/0.05937,0.00013/0.03906,0.000026/0.014787,0.000007/0.003806,0.000002/0.001181,0.0000009/0.0004290,0.0000004/0.0001787,0.00000025/0.00008152,0.00000025/0.00002216,0.00000006/0.000004653,0.000000023/0.000001402,0.000000011/0.0000003180,0.0000000004/0.00000006850,0.00000000011/0.00000001971,0.00000000002/0.000000006013,0.000000000009/0.000000001910,0.000000000004/0.0000000007181,0.0000000000020/0.0000000002083,0.0000000000007/0.00000000005311,0.00000000000031/0.00000000001639,0.00000000000016/0.000000000005354,0.000000000000073/0.000000000001709,0,0,0,0,0,0,0,0,0 };
+  TH1D * RpPb = new TH1D("pp_interpolation",";pt;E#frac{d^{3}N}{dPtdyd#phi}",s.ntrkBins,s.xtrkbins);
+  TH1D * RpPbSpectrum = new TH1D("RpPb_interp",";pt;RpPb",s.ntrkBins,s.xtrkbins);
+  TH1D * pPbSpectrum = new TH1D("pPb_data",";pt;E#frac{d^{3}N}{dPtdyd#phi}",s.ntrkBins,s.xtrkbins);
+  for(int i=1; i<s.ntrkBins+1; i++) RpPb->SetBinContent(i,ppintyvals[i-1]);
+  for(int i=1; i<s.ntrkBins+1; i++) RpPb->SetBinError(i,TMath::Power(TMath::Abs(TMath::Power(ppintyerrRpPb[i-1],2)-TMath::Power(ppintyerr_pPb[i-1],2)),0.5)*ppintyvals[i-1]);
+  for(int i=1; i<s.ntrkBins+1; i++) RpPbSpectrum->SetBinContent(i,ppintyvals_RpPb[i-1]);
+  for(int i=1; i<s.ntrkBins+1; i++) RpPbSpectrum->SetBinError(i,ppintyvals_RpPb[i-1]*ppintyerrRpPb[i-1]);
+  for(int i=1; i<s.ntrkBins+1; i++) pPbSpectrum->SetBinContent(i,ppintyvals_pPb[i-1]);
+  for(int i=1; i<s.ntrkBins+1; i++) pPbSpectrum->SetBinError(i,ppintyvals_pPb[i-1]*ppintyerr_pPb[i-1]);
+  
+  pp->Divide(RpPb);
+  float ppScale = 1/pp->GetBinContent(1);
+  pp->Scale(ppScale);
+  c1->SetLogy(0);
+  pp->GetYaxis()->SetTitle("data/interp");
+  pp->Draw(); 
+  c1->SaveAs("plots/pp_dataVsInterp.png");
 }
