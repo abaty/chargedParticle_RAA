0c02b9a96ddfe2af527d9f7e0f7dbfa9e5e5b181
Austin <abaty@mit.edu>
2016-05-09 13:50:23 -0400

    some updates for running over full MB dataset

--- submit_pp_MITOnly.sh
+++ submit_pp_MITOnly.sh
@@ -5,19 +5,17 @@ then
 fi
 
 now="Rpp_$(date +"%Y_%m_%d__%H_%M_%S")"
-njobs=1800
+njobs=300
 
 mkdir $now
 cp fileLists/testFileList.txt $now 
-cp fileLists/pp_MB_VanderbiltAllPDs.txt $now/fileList.txt 
+cp fileLists/pp_NotAtVanderbilt.txt $now/fileList.txt 
 cp -r TrkCorr_* $now
 cp -r chi2Corrector $now
 tar -cvzf $now/Corrections.tar.gz TrkCorr_* chi2Corrector
 cp run.sh $now
 
 cat run.condor | sed "s@log_flag@$now@g" | sed "s@dir_flag@$PWD/$now@g" | sed "s@user_flag@$USER@g" |  sed "s@arglist@$njobs 1@g" | sed "s@transfer_filelist@run.exe@g" | sed "s@njobs@$njobs@g" > $now/run.condor
-
-cat run.sh | sed "s@/temporaryStorage/@/temporaryStorage/ppFromVanderbilt/@g" > $now/run.sh
 
 NAME="countTracks.C"
 g++ $NAME $(root-config --cflags --libs) -Wall -O2 -o "run.exe"
